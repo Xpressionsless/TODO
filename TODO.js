5e6aa03b7cc03fe90acf85b6d3853fedeefa7233
imran khan <imrankhanswati@gmail.com>
2019-02-12 17:38:10 +0100

    Stage 09 is clear

--- TODO.js
+++ TODO.js
@@ -2,31 +2,40 @@ var item = "";
 $(document).ready(function(){
     $("#todo-form").submit(function(event){ 
         item = $("#todo-list-item").val();
-        listingFunction(item)
+        appendingFuntion(item)
         this.reset();
         event.preventDefault()
     })
+    remove()
+    checkbox()
 })
 
-function listingFunction(item) {
+function appendingFuntion(item) {
     $("#todo-items").append(appendToList(item));
 }
 
 function appendToList(item) {
-    // var $input = $("<input/>").attr({type: 'checkbox', name:'check'}).addClass('checkbox')
-    // var $link = $("<a>").attr({value: 'X', type: 'button', name: 'del'}).addClass('remove')
     var inputItem = "";
     if ( item === '') {
-        window.alert("Your must enter something")
+        window.alert("You must enter something")
         return false
     }
     else {
-        inputItem = "<li id='listItemID'><input id='label' type='checkbox'>" + item + "<a type='link' id='remove'>X</a>"
+        inputItem =  "<li class='completed'; id='select'><input id='label'; class='checkbox'; type='checkbox'>" + item + "<a href='javascript:void(0);' class='remove'>&times;</a></li>"
     }
     return inputItem;
 }
 
-function remove(item) {
 
-    $(this).closest(item).remove();
+function remove() {
+   $(document).on("click", "a.remove" , function() {
+    $(this).parent().remove();
+});
+}
+
+function checkbox() {
+    var liAddedClass = $("<li>").css(".completed")
+    $(document).on("change", "#label", function () {
+    $(this).parent().toggleClass(liAddedClass);
+      });
 }
