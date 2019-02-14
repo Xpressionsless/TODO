0bbd4596d3b86139cabf6eb74e0db00f1f2454e8
imran khan <imrankhanswati@gmail.com>
2019-02-14 23:39:18 +0100

    Just to put the property of checkbox true or false to checkbox dynamically

--- TODO.js
+++ TODO.js
@@ -11,13 +11,12 @@ $(document).ready(function () {
         checkbox()
         this.reset();
     })
-    //  remove()
-    //  checkbox()
+    checkbox()
 })
 
 class Todo {
     constructor(name) {
-        //this.id = id
+        this.checked = false
         this.name = name
     }
 }
@@ -62,10 +61,23 @@ function listTodo() {
     for (const i in todos) {
         var todo = todos[i]
         var name = todo.name
-        html += "<li class='completed'; id='select'><input id='label'; onClick='checkbox()'; type='checkbox'>" + name + "<a href='javascript:void(0)'; onClick='remove()'; class='remove'>&times;</a></li>"
+        var checkbox = todo.checked
+        html += '<li class="completed";>' + '<input id="label"; class="checkbox"; type="checkbox"; name="checkbox-name">' + name 
+        + '<a href="javascript:void(0)"; onClick="remove()"; class="remove">&times;</a></li>'
     }
     $("#todo-items").html(html)
 }
+
+// $(document).on('change', '#label', function () {
+//     for (const i in todos) {
+//         var todo = todos[i]
+//         var name = todo.name
+//         var checkbox = todo.checked
+//     $(this).prop(['checked'], checkbox);
+//     }
+//     //saveTodos()
+//    // listTodo()
+// })
 
 function remove() {
     $("#todo-items").on("click", "a.remove", function () {
@@ -78,11 +90,37 @@ function remove() {
     })
 }
 
+// Nedan två metoder har jag provat att kunna spara checkbox check i localStorage men ingen funkar perfekt
 function checkbox() {
-    //var isChecked = $("input#label").prop('checked')
-    console.log($("input#label:checked").val());
-    var liAddedClass = $("<li>").css(".completed")
-    $(document).on("change", "#label", function () {
-        $(this).parent().toggleClass(liAddedClass);
+    //var liAddedClass = $("<li>").css(".completed")
+    $('.checkbox').on('change', function () {
+        var index = $('.checkbox').index(this)
+        $(this).parent().toggleClass($("<li>").css(".completed"));
+        var isChecked
+        $(this).each(function () {
+            if (this.checked) {
+                isChecked = todos[index]['checked'] = this.checked
+                console.log('Checkbox value is: ' + isChecked)
+            } else {
+                if(todos[index]['checked'] = true) {
+                isChecked = todos[index]['checked'] = false
+                console.log('Checkbox value is: ' + isChecked)
+                }
+            }
+            //listTodo()
+            saveTodos()
+        })
     })
 }
+
+// $(document).on('change', "input[name='checkbox-name']", function (e) {
+//     if ($(this).prop('checked')) {
+//       $(this).attr('checked', 'checked');
+//     } else {
+//       $(this).removeAttr('checked');
+//     }
+    
+//     var list = $("#todo-items").html();
+//     localStorage.setItem('todes', list);
+//     return false;
+//   });
