ceb936bb2a2961dc0897b9a5fb2c35b60a062588
imran khan <imrankhanswati@gmail.com>
2019-02-14 16:29:46 +0100

    Stage 11 is clear

--- TODO.js
+++ TODO.js
@@ -1,41 +1,88 @@
-var item = "";
-$(document).ready(function(){
-    $("#todo-form").submit(function(event){ 
-        item = $("#todo-list-item").val();
-        appendingFuntion(item)
-        this.reset();
+var html = ""
+var todos = []
+
+$(document).ready(function () {
+    $("#todo-form").submit(function (event) {
         event.preventDefault()
+        item = $("#todo-list-item").val();
+        addNewTodoWithName(item)
+        listTodo()
+        remove()
+        checkbox()
+        this.reset();
     })
-    remove()
-    checkbox()
+    //  remove()
+    //  checkbox()
 })
 
-function appendingFuntion(item) {
-    $("#todo-items").append(appendToList(item));
+class Todo {
+    constructor(name) {
+        //this.id = id
+        this.name = name
+    }
 }
-
-function appendToList(item) {
-    var inputItem = "";
-    if ( item === '') {
+function addNewTodoWithName(name) {
+    var inputItem = new Todo(name)
+    if (name === "") {
         window.alert("You must enter something")
         return false
     }
-    else {
-        inputItem =  "<li class='completed'; id='select'><input id='label'; class='checkbox'; type='checkbox'>" + item + "<a href='javascript:void(0);' class='remove'>&times;</a></li>"
+    todos = todos || []
+    todos.push(inputItem)
+    saveTodos()
+}
+// REmove todo
+function removeTodoAtIndex(index) {
+    todos.splice(index, 1)
+    saveTodos()
+}
+// Get Todo
+function getTodoAtIndex(index) {
+    return todos[index]
+}
+//Save data to localStorage
+function saveTodos() {
+    var str = JSON.stringify(todos)
+    localStorage.setItem("todos", str)
+}
+// Get data from LocalStorage
+function getTodos() {
+    var str = localStorage.getItem("todos")
+    if (!todos) {
+        todos = []
+    } else {
+        todos = JSON.parse(str)
     }
-    return inputItem;
+}
+getTodos()
+listTodo()
+// list Todos
+function listTodo() {
+    html = ""
+    for (const i in todos) {
+        var todo = todos[i]
+        var name = todo.name
+        html += "<li class='completed'; id='select'><input id='label'; onClick='checkbox()'; type='checkbox'>" + name + "<a href='javascript:void(0)'; onClick='remove()'; class='remove'>&times;</a></li>"
+    }
+    $("#todo-items").html(html)
 }
 
-
 function remove() {
-   $(document).on("click", "a.remove" , function() {
-    $(this).parent().remove();
-});
+    $("#todo-items").on("click", "a.remove", function () {
+        var index = $(this).closest(".completed").index();
+        if (index > -1) {
+            todos.splice(index, 1)
+        }
+        listTodo()
+        saveTodos()
+    })
 }
 
 function checkbox() {
+    //var isChecked = $("input#label").prop('checked')
+    console.log($("input#label:checked").val());
     var liAddedClass = $("<li>").css(".completed")
     $(document).on("change", "#label", function () {
-    $(this).parent().toggleClass(liAddedClass);
-      });
+        $(this).parent().toggleClass(liAddedClass);
+    })
 }
